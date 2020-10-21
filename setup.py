cc0b1f47e28bcaac33473f01d9cd956c3c24206a
Malcolm Greaves <malcolm.greaves@scale.com>
2020-10-21 11:24:06 -0700

    Set torch build-time dependency
    Sets `torch` as a build-time dependency in `pyproject.toml` in `requires`
    under the `build-system` section.
    
    NOTE: Due to difficulties in configuring this build-time dependency to
    respect what users have in their requirements (e.g. torch 1.4.0, 1.5.1,
    or 1.6.0) and my need to have this working for 1.5.1, this dep is set to
    be < 1.6.0. (TODO: enable users to override this dependency).

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 from torch.utils.hipify import hipify_python
 
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
+with open(os.path.join(os.environ['HOME'], 'TORCH_VER_INSTALL'), 'wt') as wt: 
+    wt.write(f"\n\ntorch version: {torch_ver}\n\n")
 assert torch_ver >= [1, 4], "Requires PyTorch >= 1.4"
 
 
